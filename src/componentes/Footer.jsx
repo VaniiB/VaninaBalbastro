5ca8f6f98a52d59bb7eb745479236799e95997b6
Vanina <vaniibalbastro@gmail.com>
2024-08-08 17:49:07 -0300

    footer agregado y arreglar detalles. falta responsive

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,41 +1,13 @@
-
-import '../css/footer.css'; // Archivo CSS para estilos del footer
+import React from 'react';
+import { MDBFooter } from 'mdb-react-ui-kit';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'; // Importa los estilos de MDBootstrap
 
 export const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h3>Información de contacto</h3>
-            <p>Dirección: 123 Calle Principal, Ciudad</p>
-            <p>Teléfono: +123 456 789</p>
-            <p>Email: contacto@ejemplo.com</p>
-          </div>
-          <div className="col">
-            <h3>Enlaces útiles</h3>
-            <ul>
-              <li><a href="/">Inicio</a></li>
-              <li><a href="/about">Acerca de nosotros</a></li>
-              <li><a href="/services">Servicios</a></li>
-              <li><a href="/contact">Contacto</a></li>
-            </ul>
-          </div>
-          <div className="col">
-            <h3>Redes sociales</h3>
-            <ul className="social-icons">
-              <li><a href="https://facebook.com"><i className="fab fa-facebook"></i></a></li>
-              <li><a href="https://twitter.com"><i className="fab fa-twitter"></i></a></li>
-              <li><a href="https://instagram.com"><i className="fab fa-instagram"></i></a></li>
-            </ul>
-          </div>
-        </div>
+    <MDBFooter bgColor='light' className='text-center'>
+      <div className='text-center p-3' style={{ backgroundColor: '#232323', color:'#ffffff' }}>
+        &copy; {new Date().getFullYear()} Diseñado y desarrollado por Vanina Balbastro. Todos los derechos reservados.
       </div>
-      <div className="footer-bottom">
-        <p>&copy; 2024 Nombre de tu empresa. Todos los derechos reservados.</p>
-      </div>
-    </footer>
+    </MDBFooter>
   );
 }
-
-
